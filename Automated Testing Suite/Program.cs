d4ad00a52e20215bcd82bc935fabca34ce76ff9b
Log234 <ajsivesind@gmail.com>
2016-06-07 01:57:16 +0200

    Created initial VS projects

---
+++ Automated Testing Suite/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automated_Testing_Suite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
